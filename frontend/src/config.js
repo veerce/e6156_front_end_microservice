db47a5bc74e0704f4cf8715cd47d99e564dc3e46
Manasi S <manasinsoman@gmail.com>
2023-12-19 18:46:26 -0500

    trying to figure out graphql integration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
-export const recipeURL = "http://ec2-54-164-52-42.compute-1.amazonaws.com:8011";
+export const recipeURL = "http://ec2-18-209-24-141.compute-1.amazonaws.com:8011";
 export const userURL = "http://ec2-18-232-149-42.compute-1.amazonaws.com:8011";
 export const reviewURL = "https://review-management-402504.ue.r.appspot.com";
+export const graphQL = "http://ec2-23-23-64-126.compute-1.amazonaws.com:8000/graphql";
