77f871165a834a7e1be0da9e64f8e593b9e43bcb
veerce <vc2534@columbia.edu>
2023-12-18 19:00:03 -0500

    Search query done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import LogoutButton from "./logout";
 import CuisineGenerator from "./Cuisine"
 import Recipes from './Recipes';
 import { useNavigate } from 'react-router-dom';
-import SearchComponent from "./Search"
-
-
 
 const Header = () => {
   let navigate = useNavigate(); 
@@ -19,15 +16,15 @@ const Header = () => {
   return (
     <header id="header">
       <div id="search_bar">
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Find by title or author" aria-label="Search"></input>
+        <SearchComponent />
+        {/* <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search by title, author, or ingredient" aria-label="Search"></input>
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="search_bar_button">Search</button>
-        </form>
+        </form> */}
       </div>
 
       <h1 id="main_title" onClick={routeChange}>RECIPEDIA</h1>
       <div id = "user_controls">
-        {/* <SearchComponent /> */}
         <Profile />
         <LogoutButton />
         <CuisineButton />
@@ -35,6 +32,80 @@ const Header = () => {
   </header>
   )
 }
+
+const SearchComponent = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const searchRef = useRef(null); // Ref for the search component
+
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    if (!searchQuery.trim()) {
+      setSearchResults([]);
+      setIsDropdownVisible(false);
+      return;
+    }
+
+    const apiUrl = 'http://0.0.0.0:8011/recipes';
+    try {
+      const response = await fetch(`${apiUrl}?objects_filter=${encodeURIComponent(searchQuery)}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const results = await response.json();
+      setSearchResults(results);
+      setIsDropdownVisible(true);
+    } catch (error) {
+      console.error('Error during fetch:', error);
+      setIsDropdownVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setIsDropdownVisible(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [searchRef]);
+
+  return (
+    <div ref={searchRef}>
+      <form className="form-inline my-2 my-lg-0" onSubmit={handleSearchSubmit}>
+        <input 
+          className="form-control" 
+          type="search" 
+          placeholder="Search by title, author, or ingredient" 
+          aria-label="Search" 
+          value={searchQuery} 
+          onChange={(e) => setSearchQuery(e.target.value)}
+          style={{ width: '300px' }}
+        />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit" id="search_bar_button">Search</button>
+      </form>
+      {isDropdownVisible && (
+        <div className="search-results-dropdown">
+          {searchResults.length > 0 ? (
+            searchResults.map((result, index) => (
+              <div key={index} className="search-result-item">
+                <span>{result.title} - {result.author_id}</span>
+              </div>
+            ))
+          ) : (
+            <div className="search-result-item">
+              <span>No results found</span>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
 const Profile = () => {
 
